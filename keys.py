d122b300a87551a12ccfc4335518fea0782342f2
gksapori <gerrysaporito@gmail.com>
2019-09-14 16:52:54 -0400

    Data aggreagation algorithm complete.

--- keys.py
+++ keys.py
@@ -5,6 +5,6 @@ keys = {
     'news' : 'a93fd9a824b848bca3436e761c5a0d4d',
     'json_google_cloud' : 'htn2019-f059d4b46f7d.json',
     'xe' : XecdClient('xe891090202', '8ht3qjhq8vnu0dbb91ai40p44b'),
-    'rapid-api-host': "contextualwebsearch-websearch-v1.p.rapidapi.com",
-    'rapid-api-key': "f084d5adf9msh0eafe805eae5389p148120jsn80116693e92d"
+    'rapid_api_host': "contextualwebsearch-websearch-v1.p.rapidapi.com",
+    'rapid_api_key': "f084d5adf9msh0eafe805eae5389p148120jsn80116693e92d"
 }
